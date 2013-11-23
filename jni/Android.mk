ef6cc0035d413ddc1e6c4f0d649211f77d9fe898
Dan Stahlke <dan@stahlke.org>
2013-11-23 14:58:05 -0500

    eigen from current dir

--- jni/Android.mk
+++ jni/Android.mk
@@ -6,9 +6,6 @@ LOCAL_MODULE    := rdnlib
 LOCAL_SRC_FILES := rdnlib.cpp
 LOCAL_LDLIBS    := -lm -llog -ljnigraphics
 LOCAL_CFLAGS    := -ffast-math -O3 -funroll-loops -Wall
-# FIXME
-LOCAL_C_INCLUDES := /home/dstahlke/Desktop/eigen-android
-#LOCAL_C_INCLUDES += $(NDK_ROOT)/sources/cxx-stl/gnu-libstdc++/4.7/include/
-#LOCAL_C_INCLUDES += $(NDK_ROOT)/sources/cxx-stl/gnu-libstdc++/4.7/libs/armeabi-v7a/include/
+LOCAL_C_INCLUDES := eigen-android
 
 include $(BUILD_SHARED_LIBRARY)
